bf84460dad27cb1082031f7ab7b572dac88bfc9e
daniel otanez <111538726+Woody4M@users.noreply.github.com>
2022-09-08 04:49:32 +0000

    why not

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// this javascript file is loaded by node.js and runs on the server-side
 
+// load the core node http module
 const http = require("http");
 
+// load the core node filesystem (fs) module, using js promises instead of callbacks
 const fs = require("fs").promises;
 
+// create a function to respond to http requests
 function requestListener(req, res) {
 
+  // check the request url and return a file's contents
   const urlarray = req.url.split('/');
   console.log(urlarray);
 
@@ -28,24 +33,26 @@ function requestListener(req, res) {
       res.writeHead(404);
       res.end();
       break;
-    default:
+    default: // any other url including just root "/"
+      // load file from server filesystem
       fs.readFile(__dirname + "/public/" + urlarray[1])
         .then(contents => {
-
+            // set http response header entry text/html
             res.setHeader('Content-Type', 'image/jpeg');
-
+            // return 200 OK http status code
             res.writeHead(200);
-
+            // send back file contents + close response
             res.end(contents);
           }
         );
   }
 }
 
-
+// create an http server instance
 const server = http.createServer(requestListener);
 
-
+// call the listen() method to start listening to http requests
+// 1: port, 2: ip, 3: function to run when the server starts
 server.listen(
   8080,
   "0.0.0.0",
@@ -54,15 +61,17 @@ server.listen(
   }
 );
 
+// perform file reading for any file and mime 
+// then return using response object res
 function loadReturn(fname, ctype, res) {
-
+  // load file from server filesystem
   fs.readFile(__dirname + "/" + fname)
     .then(contents => {
-
+        // set http response header entry text/html
         res.setHeader("Content-Type", ctype + '; charset=UTF-8');
-
+        // return 200 OK http status code
         res.writeHead(200);
-
+        // send back file contents + close response
         res.end(contents);
       }
     );
