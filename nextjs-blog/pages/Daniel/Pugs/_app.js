bbc4567b0cc246cc772f532946195732b2ca6633
daniel otanez <111538726+Woody4M@users.noreply.github.com>
2022-09-13 19:05:20 +0000

    updtae

---
+++ nextjs-blog/pages/Daniel/Pugs/_app.js
@@ -0,0 +1,10 @@
+import '../style/globals.css'
+import Layout from'../comps/Layout'
+function Myapp({ Component, pagePros}) {
+  return(
+    <Layout>
+      <Component{...pageProps}
+      </Layout>
+  )
+}
+export default Myapp
